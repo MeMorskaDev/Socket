694dc0c4239fb5016b96b73bda3ca2d932aa7d1c
Gouravmoy Mohanty <gouravmoy.mohanty91@gmail.com>
2016-09-27 03:14:02 +0530

    MenuStructure Post complete with validation

--- common/models/menu/menu-structure.js
+++ common/models/menu/menu-structure.js
@@ -1,17 +1,27 @@
 var loopback = require('loopback');
 module.exports = function MenuStructure(MenuStructure) {
     MenuStructure.observe('before save', function (ctx, next) {
-        var menuStructure = ctx.data || ctx.instance;
-        if (menuStructure.parentId != 'null') {
+        var inputMenuStructure = ctx.data || ctx.instance;
+        if (inputMenuStructure.__data.parentId != 'null') {
             var MenuStructure = loopback.getModel('MenuStructure');
-            MenuStructure.findById(menuStructure.parentId, function (err, menuStructure) {
+            MenuStructure.findById(inputMenuStructure.__data.parentId, function (err, menuStructure) {
                 if (err) {
                     console.log("Error in creating MenuStructure" + err);
                     return next(err);
                 } else {
                     if (menuStructure) {
-                        console.log("MenuStructure verified");
-                        next();
+                        MenuStructure.find({ where: { 'parentId': inputMenuStructure.__data.parentId } }, function (err, subMenus) {
+                            subMenus.forEach(function (subMenu) {
+                                //console.log("Sub Catagoery " + subMenu.categoryName + " " + inputMenuStructure.__data.parentId);
+                                if (subMenu.categoryName === inputMenuStructure.__data.categoryName) {
+                                    //console.log("Error here");
+                                    next(new Error('Error in creating MenuStructure as Sub Menu Exists'));
+                                }
+                            });
+                            console.log("MenuStructure verified");
+                            next();
+                        });
+
                     }
                     else {
                         console.log("Error in creating MenuStructure as parentId does not Exist");
